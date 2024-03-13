45c63d18983a84c918a2b10be653d13e4e8d0efc
vrushabhekhande28 <vrushabhekhande28@gmail.com>
2024-03-13 22:11:52 +0530

    first time commit

---
+++ src/test/java/gitandgithublearn/GitAndGitHub.java
@@ -0,0 +1,18 @@
+package gitandgithublearn;
+
+public class GitAndGitHub 
+{
+  public void test()
+  {
+	  int percentage=10;
+	  
+	   if(percentage>35)
+	   {
+		   System.out.println("you are failed");
+	   }
+	   else if(percentage> 35 && percentage< 85)
+	   {
+		    System.out.println("You are passed with first class");
+	   }
+  }
+}
