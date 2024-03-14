ccb2afce6f687d2f30479e121f4c4de89aeeb528
vrushabhekhande28 <vrushabhekhande28@gmail.com>
2024-03-14 11:28:05 +0530

    new changes created new class is there.

---
+++ src/test/java/gitandgithublearn/gitandgithublearn_02.java
@@ -0,0 +1,21 @@
+package gitandgithublearn;
+
+public class gitandgithublearn_02
+{
+  
+  public void test2()
+   { 
+	   int per=100;
+	   
+	   if(per>60)
+	   {
+		  System.out.println("you can allow to fill the form");   
+	   }
+	   
+	   else
+	   {
+		System.out.println("you are not allowed");
+	   }
+   }
+
+}
